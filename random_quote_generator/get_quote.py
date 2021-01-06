17ba0879644bc4046e8ceff4afa9dabc5dabe4b8
Prathan D <zyracuze@gmail.com>
2021-01-07 01:49:24 +0700

    [CREATED] get_quote.py

---
+++ random_quote_generator/get_quote.py
@@ -0,0 +1,15 @@
+import random
+
+from random_quote_generator.quotes import quotes
+
+def get_quote() -> dict:
+    """
+    Get random quote
+
+    Get randomly selected quote from database our programming quotes
+
+    :return: selected quote
+    :rtype: dict
+    """
+
+    return quotes[random.randint(0, len(quotes) - 1)]
